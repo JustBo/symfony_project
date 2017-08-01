db603e3de4ad646e409e185ed6b189fe4da1c1a0
justbo <b.tsybulskyy@gmail.com>
2017-08-01 15:33:42 +0300

    added css,js ; quried from database

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -6,12 +6,16 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 //use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Blog;
 
 class HomeController extends Controller{
     /**
      * @Route("/", name="homepage")
      */
     public function showAction(){
-        return $this->render('home/index.html.twig');
+        $blogs = $this->getDoctrine()->getRepository(Blog::class)->findAll();
+        return $this->render('home/index.html.twig',[
+          'blogs' => $blogs
+        ]);
     }
 }
