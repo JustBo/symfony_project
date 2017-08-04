cf3619b1442e1148aca06dab520598d5549d345d
justbo <b.tsybulskyy@gmail.com>
2017-08-04 18:59:07 +0300

    added basic list for blogs

--- src/AppBundle/Admin/BlogPostAdmin.php
+++ src/AppBundle/Admin/BlogPostAdmin.php
@@ -4,6 +4,7 @@
   use Sonata\AdminBundle\Admin\AbstractAdmin;
   use Sonata\AdminBundle\Datagrid\ListMapper;
   use Sonata\AdminBundle\Form\FormMapper;
+  use Sonata\AdminBundle\Datagrid\DatagridMapper;
 
   class BlogPostAdmin extends AbstractAdmin {
 
@@ -23,13 +24,28 @@
     }
 
     protected function configureListFields(ListMapper $listMapper) {
-        //$listMapper->addIdentifier('name');
-    }
 
+        $listMapper
+          ->addIdentifier('title')
+          ->add('category.name')
+          ->add('draft')
+        ;
+    }
+    //on succes
     public function toString($object) {
         return $object instanceof BlogPost
             ? $object->getTitle()
             : 'Blog Post'; // shown in the breadcrumb on the create view
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper) {
+        $datagridMapper
+          ->add('title')
+          ->add('category', null, array(), 'entity', array(
+            'class' => 'AppBundle\Entity\Category',
+            'choice_label' => 'name'
+          ))
+          ;
     }
 
  }
