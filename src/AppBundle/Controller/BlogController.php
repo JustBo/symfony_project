890e2d1e128bf56d0882fb8f52d89067c40eee74
justbo <b.tsybulskyy@gmail.com>
2017-08-16 19:04:33 +0300

    add styles

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 //use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\BlogPost;
-//use AppBundle\Repository\BlogRepository;
+use AppBundle\Repository\BlogPostRepository;
 
 class BlogController extends Controller{
     /**
@@ -21,4 +21,14 @@ class BlogController extends Controller{
           'blogs' => $blogs
         ]);
     }
+
+    public function showFooterPostsAction(){
+        $repository = $this->getDoctrine()->getRepository(BlogPost::class);
+        $recentBlogs = $repository->findRecentPosts(3);
+        // $blogs = $repository->findAll();
+        return $this->render('footer/blogs.html.twig',[
+          'recentBlogs' => $recentBlogs
+        ]);
+    }
+
 }
